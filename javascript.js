ed65bae114ea6e2a95024c5179671a1a8e4f6f41
cstevens <colestevens24@gmail.com>
2022-05-24 08:31:48 -0700

    working rock paper scissors function

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ console.log("testing")
 let randomNumber = Math.floor(Math.random() * 3) + 1;
 
 // randomly picks rock, paper, or scissors based on randomNumber
-let computerPlay = function() {
+let cpuPlay = function() {
    if (randomNumber === 1) {
        return("rock")
    } else if (randomNumber === 2) {
@@ -14,17 +14,57 @@ let computerPlay = function() {
    } else {
        alert("Something went wrong...")
    }
-}
-
-//asks for user input of rock/paper/scissors
-let userSelection = prompt("Please type rock, paper, or scissors.", "")
-
-//stores user input of rock/paper/scissors in lowercase to make input case-insensitive
-let userChoice = userSelection.toLowerCase()
-
-//  console.log(computerPlay())
-console.log(userChoice)
+};
 
 //store computerPlay output in variable computerChoice
+let cpuChoice = cpuPlay();
+
+//asks for user input of rock/paper/scissors
+let userSelection = prompt("Please type rock, paper, or scissors.");
+
+//stores user input of rock/paper/scissors in lowercase to make input case-insensitive
+let userChoice = userSelection.toLowerCase();
+
+// win statement stencil
+let win = `You win! ${userChoice} beats ${cpuChoice}!`;
+
+// loss statement stencil
+let loss = `You lose! ${cpuChoice} beats ${userChoice}!`;
+
+//takes cpuChoice and userChoice, then outputs string declaring winner.
+let singleRound = function () {
+    if (userChoice === cpuChoice) {
+        return(`It's a Draw! You both chose ${userChoice}!`)
+    } else if (userChoice === "rock") {
+        if (cpuChoice === "scissors") {
+            return(win)
+        } else if (cpuChoice === "paper") {
+            return(loss)
+        }
+    } else if (userChoice === "paper") {
+        if (cpuChoice === "rock") {
+            return(win)
+        } else if (cpuChoice === "scissors") {
+            return(loss)
+        }
+    } else if (userChoice === "scissors") {
+        if (cpuChoice === "paper") {
+            return(win)
+        } else if (cpuChoice === "rock") {
+            return(loss)
+        }
+    } else {
+        return("Oops! Looks like you mispelled your choice!")
+    }
+}
+
+console.log("second testing")
+
+//easy variable tests
+console.log("Computer chose " + cpuChoice)
+console.log("User chose " + userChoice)
+
+console.log(singleRound());
+
 
 
